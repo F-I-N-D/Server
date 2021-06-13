001f1dc04dc5e0a360d6c4a45ae56f3a5ec19f05
Nick van Leeuwen <nickvanleeuwen01@gmail.com>
2021-06-13 12:04:14 +0200

    Added command line options

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import argparse
+import sys
+
 from Server.Server import Server
+
+# Create argument parser for command line arguments
+parser = argparse.ArgumentParser(description="Options for the swarm server.")
+parser.add_argument('-camera', '-c', type = int, default = 0, help = "define the GPS camera index")
+parser.add_argument('-noGui', choices=['search', 'calibrate', 'scatter'], help = "run action without starting the GUI")
 
 # Create server instance and start the server
 # This file needs te be ran directly and can't be inported
 if __name__ == '__main__':
-    server = Server()
+    args = parser.parse_args(sys.argv[1:])
+    server = Server(args.camera, args.noGui)
     server.start()

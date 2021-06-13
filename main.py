4bb5ab493f6867fd794c04981e4eb4a9eff6b1d5
Nick van Leeuwen <nickvanleeuwen01@gmail.com>
2021-06-13 12:05:24 +0200

    Changed command line input names

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from Server.Server import Server
 
 # Create argument parser for command line arguments
 parser = argparse.ArgumentParser(description="Options for the swarm server.")
-parser.add_argument('-camera', '-c', type = int, default = 0, help = "define the GPS camera index")
-parser.add_argument('-noGui', choices=['search', 'calibrate', 'scatter'], help = "run action without starting the GUI")
+parser.add_argument('--camera', '-c', type = int, default = 0, help = "define the GPS camera index")
+parser.add_argument('--noGui', choices=['search', 'calibrate', 'scatter'], help = "run action without starting the GUI")
 
 # Create server instance and start the server
 # This file needs te be ran directly and can't be inported
